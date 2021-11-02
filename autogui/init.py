c701aed5ed3058f6f9f00ce23bd5233391fcfefe
Rohit Sharma <rohitsdec4@gmail.com>
2021-11-02 15:58:35 +0530

    use get request to fetch open airdrops list

--- autogui/init.py
+++ autogui/init.py
@@ -1,10 +1,13 @@
+from bs4 import BeautifulSoup
 from configs import get_configs
 import logging
 import pyautogui
-from .utils import Position
-import webbrowser
 import pyperclip
-from bs4 import BeautifulSoup
+import requests
+import webbrowser
+
+from .utils import Position
+
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
@@ -37,28 +40,12 @@ class GUI:
         pyautogui.sleep(3)
         logging.info(f'User with email: {email}, logged in')
 
-    def open_coindrop_page(self):
-        pyautogui.hotkey('ctrl', 'l')
-        pyautogui.write(self.configs['coinmarketcap']['airdrop_url'])
-        pyautogui.press('enter')
-        pyautogui.sleep(2)
-
-        pyautogui.hotkey('ctrl', 'u')
-        pyautogui.sleep(1)
-        pyautogui.hotkey('ctrl', 'a', inteval=0.15)
-        pyautogui.hotkey('ctrl', 'c', inteval=0.15)
-        text = pyperclip.paste()
-        pyautogui.hotkey('ctrl', 'w', inteval=0.15)
-
-        soup = BeautifulSoup(text, 'html.parser')
-        tds = soup.find_all('td', attrs={'style': 'text-align:left'})
-        # TODO: write tds to a file
-        list_of_airdrops = [(td.a['href'], td.select_one('span').text) for td in tds]
-        for airdrop in list_of_airdrops:
-            pyautogui.hotkey('ctrl', 'l')
-            pyautogui.write(self.configs['coinmarketcap'] + airdrop[0])
-            pyautogui.press('enter')
-            pyautogui.sleep(2)
-
-            self.position.scroll('COINMARKET__JOIN_AIRDROP', )
-
+    def retrieve_open_airdrops_list(self):
+        res = requests.get(self.configs['coinmarketcap']['airdrop_url'])
+        if res.ok:
+            html = res.text
+            soup = BeautifulSoup(html, 'html.parser')
+            tds = soup.find_all('td', attrs={'style': 'text-align:left'})
+            # TODO: write airdrops to a file
+            list_of_airdrops = [(td.a['href'], td.select_one('span').text) for td in tds]
+            print(list_of_airdrops)
